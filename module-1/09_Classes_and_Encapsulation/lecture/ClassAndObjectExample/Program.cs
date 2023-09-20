ff6fab3dbd713d939817b2eb8102f68dd6b22e78
John Fulton <john@techelevator.com>
2023-09-20 15:37:18 -0400

    module-1/09_Classes_and_Encapsulation/

---
+++ module-1/09_Classes_and_Encapsulation/lecture/ClassAndObjectExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassAndObjectExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
