777e6e8284ab416cd06902a1b1815d65238c80d9
John Fulton <john@techelevator.com>
2023-09-20 13:20:21 -0400

    module-1/08_Collections_Part_2_Dictionaries/

---
+++ module-1/08_Collections_Part_2_Dictionaries/lectureWithJohnsChanges/CollectionsPart2Lecture/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace CollectionsPart2Lecture
+{
+    public class CollectionsPart2Lecture
+	{
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("####################");
+            Console.WriteLine("       HASHSET");
+            Console.WriteLine("####################");
+            Console.WriteLine();
+
+            HashSet<string> names = new HashSet<string>();
+            names.Add("John");
+            names.Add("John");
+            names.Add("Nancy");
+            names.Add("Lisa");
+
+            List<string> myList = names.ToList(); ;
+
+
+            Console.WriteLine("####################");
+			Console.WriteLine("       DICTIONARIES");
+			Console.WriteLine("####################");
+			Console.WriteLine();
+
+            Dictionary<string, int> locationA = new Dictionary<string, int>();
+            locationA["a234"] = 7;
+            locationA["br123"] = 3;
+            locationA["t56"] = 19;
+
+            Dictionary<string, int> locationB = new Dictionary<string, int>();
+            locationB["b234"] = 7;
+            locationB["xy123"] = 3;
+            locationB["t56"] = 19;
+
+
+            Dictionary<string, int> inventory = new Dictionary<string, int>();
+
+            foreach(string itemKey in locationA.Keys)
+            {
+                inventory[itemKey] = locationA[itemKey];
+            }
+
+            foreach (string itemKey in locationB.Keys)
+            {
+                if (inventory.ContainsKey(itemKey))
+                {
+                    inventory[itemKey] = inventory[itemKey] + locationB[itemKey];
+                }
+                else
+                {
+                    inventory[itemKey] = locationB[itemKey];
+                }
+            }
+        }
+	}
+}
