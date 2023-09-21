179a5957ac0bca8b0cc4fa8848c53e8ee0f38664
John Fulton <john@techelevator.com>
2023-09-21 13:56:35 -0400

    module-1/09_Classes_and_Encapsulation/

---
+++ module-1/09_Classes_and_Encapsulation/lectureWithJohnsChanges/ClassAndObjectExample/Pet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndObjectExample
+{
+    public class Pet
+    {
+        private string ownerName;
+        public string PetName { get; set; }
+
+        public string Owner
+        {
+            get
+            {
+                return ownerName;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    ownerName = "unknown";
+                }
+                else
+                {
+                    ownerName = value;
+                }
+            }
+        }
+
+        public Pet()
+        {
+
+        }
+
+        public Pet(string petName)
+        {
+            PetName = petName;
+        }
+
+        public string PetInfo()
+        {
+            return PetName + " - " + Owner;
+        }
+    }
+}
