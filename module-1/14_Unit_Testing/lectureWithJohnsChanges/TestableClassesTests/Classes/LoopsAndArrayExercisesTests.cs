249a958d0882d3d1b196bccbeb1e69699be9d100
John Fulton <john@techelevator.com>
2023-09-28 13:40:45 -0400

    module-1/14_Unit_Testing/

---
+++ module-1/14_Unit_Testing/lectureWithJohnsChanges/TestableClassesTests/Classes/LoopsAndArrayExercisesTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestableClasses.Classes.Tests
+{
+    [TestClass()]
+    public class LoopsAndArrayExercisesTests
+    {
+        //CollectionAssert
+        //.AllItemsAreNotNull() - Looks at each item in actual collection for not null
+        //.AllItemsAreUnique() - Checks for uniqueness among actual collection
+        //.AreEqual() - Checks to see if two collections are equal (same order and quantity)
+        //.AreEquilavent() - Checks to see if two collections have same element in same quantity, any order
+        //.AreNotEqual() - Opposite of AreEqual
+        //.AreNotEquilavent() - Opposite or AreEqualivent
+        //.Contains() - Checks to see if collection contains a value/object
+
+        [TestMethod]
+        public void MiddleWayTest()
+        {
+
+            //Arange
+            LoopsAndArrayExercises testObject = new LoopsAndArrayExercises();
+
+            //Act
+            int[] result = testObject.MiddleWay(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 });
+
+            //Assert
+            CollectionAssert.AreEqual(new int[] { 2, 5 }, result);
+        }
+
+    }
+}
