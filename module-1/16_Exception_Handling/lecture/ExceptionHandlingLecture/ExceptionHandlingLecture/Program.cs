f5aa92b0425c3c821997a4b351189d651c57bf06
John Fulton <john@techelevator.com>
2023-09-30 12:34:33 -0400

    module-1/16_Exception_Handling/

---
+++ module-1/16_Exception_Handling/lecture/ExceptionHandlingLecture/ExceptionHandlingLecture/Program.cs
@@ -0,0 +1,12 @@
+﻿using ExceptionHandlingLecture;
+using System.Reflection.Metadata;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        UserInterface ui = new UserInterface();
+        ui.Run();
+
+    }
+}
---
+++ module-1/16_Exception_Handling/lecture/ExceptionHandlingLecture/ExceptionHandlingLecture/UserInterface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingLecture
+{
+    public class UserInterface
+    {
+        public void Run()
+        {
+            Console.WriteLine("Enter 0 (zero) to divide by zero");
+
+            // could generate a FormatException
+            string userInput = Console.ReadLine();
+            int zero = int.Parse(userInput);
+
+            // could generate a DivideByZeroException
+            int result = 27 / zero;
+
+            // could generate an IndexOutOfRangeException
+            int[] anArray = new int[10];
+            anArray[result] = 34;
+
+            // we will get here only if there are no exceptions
+            Console.WriteLine("Thanks for playing our little game.");
+        }
+    }
+}
