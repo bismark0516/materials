54bc7187c3d177fcf5a88de412ef6d2104da286c
John Fulton <john@techelevator.com>
2023-10-03 19:32:42 -0400

    module-1/18_Review_Day/

---
+++ module-1/18_Review_Day/Pets_V5/Pets/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PetInfo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserInterface ui = new UserInterface();
+            ui.Run();
+        }
+    }
+}
