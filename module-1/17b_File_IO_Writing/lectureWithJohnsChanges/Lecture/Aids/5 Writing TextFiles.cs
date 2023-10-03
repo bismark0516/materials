4d821de4e1b99d64dcf7b099264b0cad8485aae9
John Fulton <john@techelevator.com>
2023-10-03 13:25:50 -0400

    module-1/17a & b

---
+++ module-1/17b_File_IO_Writing/lectureWithJohnsChanges/Lecture/Aids/5 Writing TextFiles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Lecture.Aids
+{
+    public static class WritingTextFiles
+    {
+        /*
+        * This method below provides sample code for printing out a message to a text file.
+        */
+        public static void WritingAFile()
+        {
+
+            string filepath = @"c:\niceplace\times.txt";
+
+            using (StreamWriter sw = new StreamWriter(filepath, true))
+            {
+                sw.WriteLine(DateTime.UtcNow);
+            }
+
+            // After the using statement ends, file has now been written
+            // and closed for further writing
+        }
+    }
+}
