a3885fbfb765cdf5249788e59247f2146489f2a2
Bismark Akomeah <bismarkjunior846@gmail.com>
2023-10-04 11:26:32 -0400

    module-1/18_Review_Day/

---
+++ module-1/18_Review_Day/Assessment/Assessment/Classes/TicketPurchase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment.Classes
+{
+    public class TicketPurchase
+    {
+        public string Name { get; private set; }
+        public int NumberOfTickets { get; private set; }
+
+        public decimal BasePrice
+        {
+            get
+            {
+                return NumberOfTickets * 59.99M;
+            }
+        }
+
+        public TicketPurchase(string name, int numberOfTickets)
+        {
+            Name = name;
+            NumberOfTickets = numberOfTickets;
+        }
+
+        public void TotalPrice(bool earlyCheckIn, bool priorityRideAccess)
+        {
+            decimal totalPrice = BasePrice;
+
+
+            if (earlyCheckIn == true && priorityRideAccess == true)
+            {
+                totalPrice = BasePrice + (NumberOfTickets * 10) + (NumberOfTickets * 50);
+            }
+            else if (earlyCheckIn == true)
+            {
+                totalPrice = BasePrice + (NumberOfTickets * 10);
+            }
+            else if (priorityRideAccess == true)
+            {
+                totalPrice = BasePrice + (NumberOfTickets * 50);
+            }
+            else;
+
+           
+        }
+    }
+}
--- module-1/18_Review_Day/Assessment/Assessment/Program.cs
+++ module-1/18_Review_Day/Assessment/Assessment/Program.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using Assessment.Classes;
+using System;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
 namespace Assessment
 {
-    public class Program
+    public class Program : TicketPurchase
     {
-        public static void Main(string[] args)
+        public Program(string name, int numberOfTickets) : base(name, numberOfTickets)
         {
-            // create an object and call methods on it (manual testing) from this class.
-            // You DO NOT need to prompt the user to type in any values
+
         }
+        public override string ToString()
+        {
+            string message = ($"Ticket {Name}{BasePrice}");
+            Console.WriteLine(message);
+            return message;
+        }
+
+      
+
+        public static void Main()
+        {
+            Console.WriteLine();
+
+        }
+
+
+
     }
 }
+
+
+
+
