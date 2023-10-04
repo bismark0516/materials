cf0d51c9d166077a0c746394227d2948f022e00d
John Fulton <john@techelevator.com>
2023-10-04 09:48:04 -0400

    module-1/18_Review_Day/Assessment/

---
+++ module-1/18_Review_Day/Assessment/Assessment.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assessment.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // There should be a test class to test the new class.
+            // You can rename this class or create a new class.
+            // If you create a new class, the UnitTest1 class can
+            // serve as a template for what's inside of
+            // the test class
+        }
+    }
+}
