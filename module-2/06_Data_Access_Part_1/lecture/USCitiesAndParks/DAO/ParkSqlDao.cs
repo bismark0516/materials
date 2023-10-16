26461d3bb916fd5d077859cd917d90b28db30fe3
John Fulton <john@techelevator.com>
2023-10-16 14:33:13 -0400

    module-2/06_Data_Access_Part_1/

---
+++ module-2/06_Data_Access_Part_1/lecture/USCitiesAndParks/DAO/ParkSqlDao.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using USCitiesAndParks.Models;
+
+namespace USCitiesAndParks.DAO
+{
+    public class ParkSqlDao : IParkDao
+    {
+        private readonly string connectionString;
+
+        public ParkSqlDao(string connString)
+        {
+            connectionString = connString;
+        }
+
+        public int GetParkCount()
+        {
+            return 0;
+        }
+
+        public DateTime GetOldestParkDate()
+        {
+            return DateTime.Now;
+        }
+
+        public decimal GetAverageParkArea()
+        {
+            return 0.0M;
+        }
+
+        public IList<string> GetParkNames()
+        {
+            return new List<string>();
+        }
+
+        public Park GetRandomPark()
+        {
+            return null;
+        }
+
+        public IList<Park> GetParksWithCamping()
+        {
+            return new List<Park>();
+        }
+
+        public Park GetParkById(int parkId)
+        {
+            return null;
+        }
+
+        public IList<Park> GetParksByState(string stateAbbreviation)
+        {
+            return new List<Park>();
+        }
+
+        public IList<Park> GetParksByName(string name, bool useWildCard)
+        {
+            return new List<Park>();
+        }
+
+        private Park MapRowToPark(SqlDataReader reader)
+        {
+            return new Park();
+        }
+    }
+}
