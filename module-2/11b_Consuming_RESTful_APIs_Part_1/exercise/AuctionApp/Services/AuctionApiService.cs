6b9c67f9b23828a3f4b08128b462d9cb5e4b4e1d
Bismark Akomeah <bismarkjunior846@gmail.com>
2023-10-23 12:38:52 -0400

    module-2/11b_Consuming_RESTful_APIs_Part_1/

--- module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/AuctionApp/Services/AuctionApiService.cs
+++ module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/AuctionApp/Services/AuctionApiService.cs
@@ -1,6 +1,7 @@
 ﻿using RestSharp;
 using System.Collections.Generic;
 using AuctionApp.Models;
+using System;
 
 namespace AuctionApp.Services
 {
@@ -15,22 +16,76 @@ namespace AuctionApp.Services
 
         public List<Auction> GetAllAuctions()
         {
-            throw new System.NotImplementedException();
-        }
+            List<Auction> auction = new List<Auction>();
 
+            RestRequest requestAll = new RestRequest("auctions");
+            IRestResponse<List<Auction>> response = client.Get<List<Auction>>(requestAll);
+
+            if (response.IsSuccessful)
+            {
+                auction = response.Data;
+            }
+            else
+            {
+                throw new Exception("Error" + response.ErrorMessage);
+            }
+
+            return auction;
+        }
+        
         public Auction GetDetailsForAuction(int auctionId)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest($"auctions/{auctionId}");
+            IRestResponse<Auction> response = client.Get<Auction>(request);
+
+            if (response.IsSuccessful)
+            {
+                return response.Data;
+            }
+            else
+            {
+                throw new Exception("Error" + response.ErrorMessage);
+            }
+                        
         }
 
         public List<Auction> GetAuctionsSearchTitle(string searchTerm)
         {
-            throw new System.NotImplementedException();
+            List<Auction> auction = new List<Auction>();
+
+            RestRequest request = new RestRequest($"auctions?title_like={searchTerm}");
+            //auctions?title_like=<*value*>
+            IRestResponse<List<Auction>> response = client.Get<List<Auction>>(request);
+
+            if (response.IsSuccessful)
+            {
+                auction = response.Data;
+            }
+            else
+            {
+                throw new Exception("Error" + response.ErrorMessage);
+            }
+            return auction;
+           
         }
 
         public List<Auction> GetAuctionsSearchPrice(double searchPrice)
         {
-            throw new System.NotImplementedException();
+            List<Auction> auction = new List<Auction>();
+
+            RestRequest request = new RestRequest($"auctions?currentBid_lte={searchPrice}");
+            IRestResponse<List<Auction>> response = client.Get<List<Auction>>(request);
+
+            if (response.IsSuccessful)
+            {
+                auction =  response.Data;
+            }
+            else
+            {
+                throw new Exception("Error" + response.ErrorMessage);
+            }
+
+            return auction;
         }
     }
 }
