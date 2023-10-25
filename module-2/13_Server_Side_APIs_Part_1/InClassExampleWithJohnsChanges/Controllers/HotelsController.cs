8da348d70a72112b66fc2307a295c7970ed8c11b
John Fulton <john@techelevator.com>
2023-10-25 13:37:27 -0400

    module-2/13_Server_Side_APIs_Part_1/

---
+++ module-2/13_Server_Side_APIs_Part_1/InClassExampleWithJohnsChanges/Controllers/HotelsController.cs
@@ -0,0 +1,46 @@
+﻿using HotelReservations.DAO;
+using HotelReservations.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InClassExample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+
+        // GET /hotels
+        [HttpGet()]
+        public ActionResult GetHotels(int stars = 0)
+        {
+            List<Hotel> hotels = null;
+
+            if (stars == 0)
+            {
+                IHotelDao dao = new HotelMemoryDao();
+                hotels = dao.GetHotels();
+            }
+            else
+            {
+                IHotelDao dao = new HotelMemoryDao();
+                hotels = dao.GetHotelsByStars(stars);
+
+
+            }
+
+            return Ok(hotels);
+        }
+
+
+
+        // GET /hotels/5
+        [HttpGet("{id}")]
+        public ActionResult<Hotel> GetHotel(int id)
+        {
+            IHotelDao dao = new HotelMemoryDao();
+            Hotel hotel = dao.GetHotelById(id);
+            return hotel;
+        }
+    }
+}
---
+++ module-2/13_Server_Side_APIs_Part_1/InClassExampleWithJohnsChanges/Controllers/ReservationsController.cs
@@ -0,0 +1,23 @@
+﻿using HotelReservations.DAO;
+using HotelReservations.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace InClassExample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+
+        // POST /reservations
+        [HttpPost()]
+        public ActionResult<Reservation> AddReservation(Reservation reservation)
+        {
+            IReservationDao dao = new ReservationMemoryDao();
+            Reservation newReservation = dao.CreateReservation(reservation);
+            return Ok(newReservation);
+        }
+    }
+}
