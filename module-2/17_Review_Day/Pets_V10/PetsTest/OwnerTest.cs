5f3e49efef5f50d32d1b091843e1c8d5da3698fa
John Fulton <john@techelevator.com>
2023-10-30 14:56:39 -0400

    module-2/16_Securing_APIs/

---
+++ module-2/17_Review_Day/Pets_V10/PetsTest/OwnerTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetsTest
+{
+
+    //create a Owner object and test that  the object is not null
+    [TestClass]
+    public class OwnerTest
+    {
+        [TestMethod]
+        public void OwnerObjectTest()
+        {
+            string email = "drew123@gmail.com";
+            string name = "Drew";
+            Owner testObject = new Owner(name,email);
+
+            Assert.IsNotNull(testObject);
+        }
+    }
+}
---
+++ module-2/17_Review_Day/Review/PetInfoServer/DAO/IUserDao.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using PetInfoServer.Models;
+
+namespace PetInfoServer.DAO
+{
+    public interface IUserDao
+    {
+        User GetUserById(int id);
+        User GetUserByUsername(string username);
+        User CreateUser(string username, string password);
+        IList<User> GetUsers();
+    }
+}
