e7a31e65523d0b08922d842c7327fcc5ed92ad40
John Fulton <john@techelevator.com>
2023-11-17 13:31:08 -0500

    module-3/10_Vue_Event_Handling/

---
+++ module-3/09_Intro_to_Vue_JS/lecture-final/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <product-review />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import ProductReview from './components/ProductReview.vue';
+
+export default {
+  components: {
+    HelloWorld,
+    ProductReview
+  }
+};
+</script>
+
+<style>
+.main {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 800px;
+  margin: 60px auto 0 auto;
+}
+</style>
