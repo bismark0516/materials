7e68be28965c3381bea430b064d1b88097bbfe39
John Fulton <john@techelevator.com>
2023-11-21 14:26:45 -0500

    module-3/11_Vue_Component_Communication/

---
+++ module-3/11_Vue_Component_Communication/lectureForPropsWithJohnsChanges/src/components/DisplayReview.vue
@@ -0,0 +1,24 @@
+ <template>
+ <h4>{{ item.reviewer }}</h4>
+      <div class="rating">
+        <img
+          src="../assets/star.png"
+          v-bind:title="item.rating + ' Star Review'"
+          class="ratingStar"
+          v-for="n in item.rating"
+          v-bind:key="n"
+        />
+      </div>
+
+      <h3>{{ item.title }}</h3>
+      <p>{{ item.review }}</p>
+      <p>Favorite? <input type="checkbox" 
+        v-model="item.favorited" /></p>
+        </template>
+        <script>
+
+        export default {
+
+            props: ["item"]
+        }
+        </script>
