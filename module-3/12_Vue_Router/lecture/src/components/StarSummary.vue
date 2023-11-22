098533763f83003ae5a783d1142889838241acf1
Brian Lauvray <brian@techelevator.com>
2023-11-21 20:00:49 -0500

    fixes to lecture code

--- module-3/12_Vue_Router/lecture/src/components/StarSummary.vue
+++ module-3/12_Vue_Router/lecture/src/components/StarSummary.vue
@@ -25,3 +25,26 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.well {
+  display: inline-block;
+  width: 15%;
+  border: 1px black solid;
+  border-radius: 6px;
+  text-align: center;
+  margin: 0.25rem;
+  padding: 0.25rem;
+}
+
+.amount {
+  color: darkslategray;
+  display: block;
+  font-size: 2.5rem;
+}
+
+.amount:hover {
+  cursor: pointer;
+}
+
+</style>
