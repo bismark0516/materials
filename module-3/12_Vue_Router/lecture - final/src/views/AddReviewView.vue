6cd6c6c71b9d6bae23d490e5260bc3081a48de04
Brian Lauvray <brian@techelevator.com>
2023-11-22 12:45:12 -0500

    lecture code final

---
+++ module-3/12_Vue_Router/lecture - final/src/views/AddReviewView.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Add Review</h1>
+    <AddReview v-bind:productId="currentProductId" />
+</template>
+
+<script>
+import AddReview from '@/components/AddReview.vue';
+
+export default {
+
+    components: {
+        AddReview
+    },
+    computed: {
+        currentProductId() {
+            const productIdFromUrl = this.$route.params.id;
+            return productIdFromUrl;
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ module-3/12_Vue_Router/lecture - final/src/views/ProductsView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Products</h1>
+    <ProductsList />
+</template>
+
+<script>
+import ProductsList from '@/components/ProductsList.vue';
+
+export default {
+    name: 'product-view',
+    components: {
+        ProductsList
+    }
+}
+
+
+</script>
